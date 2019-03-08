6d5d6d695f8bc877bff248888d44340275403fe5
takayuki kitsunai <takayuki.kitsunai@uzabase.com>
2019-03-08 22:56:36 +0900

    add test data / mysql version up

--- environment/mysql/init/01_initial_db.sql
+++ environment/mysql/init/01_initial_db.sql
@@ -1,3 +1,5 @@
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'test';
+ALTER USER 'test'@'%' IDENTIFIED WITH mysql_native_password BY 'test';
 
 CREATE DATABASE testdb CHARACTER SET utf8mb4;
 
@@ -6,3 +8,7 @@ CREATE TABLE testdb.user (
   name VARCHAR(20) not null,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO testdb.user values (null, 'ichiro yamada');
+INSERT INTO testdb.user values (null, 'jiro yamada');
+INSERT INTO testdb.user values (null, 'saburo yamada');
